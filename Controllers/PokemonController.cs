ba8ecce8d196c1b15b5550d98f3cfa352bef4758
MukaDeveloper <muka.bertoni@gmail.com>
2024-06-18 20:39:10 -0300

    feat: página feita

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PokemonMvc.Models;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace PokemonMvc.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public string uriBase = "http://mukabertoni.somee.com/Api/Pokemon/";
+
+        [HttpGet]
+        public async Task<ActionResult> IndexAsync()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    List<PokemonViewModel> listaPokemon = await Task.Run(() =>
+                        JsonConvert.DeserializeObject<List<PokemonViewModel>>(serialized)!);
+
+                    return View(listaPokemon);
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+
+}
--- Models/PokemonViewModel.cs
+++ Models/PokemonViewModel.cs
@@ -1,18 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 using PokemonMvc.Models.Enuns;
 
 namespace PokemonMvc.Models
 {
     public class PokemonViewModel
     {
+        //Atalho: PROP + TAB
         public int Id { get; set; }
         public string Nome { get; set; } = "";
         public double Altura { get; set; }
         public double Peso { get; set; }
-        public string Genero { get; set; } = "";
         public List<TipoEnum> Tipo { get; set; }
     }
 }
